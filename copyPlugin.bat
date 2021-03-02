f3679f8b422f2dea27edc20ffa3b393055158f86
KillerJim-DVG <JamesHunter-DeepVoodoo@users.noreply.github.com>
2021-03-02 17:02:06 +0000

    Initial commit

---
+++ copyPlugin.bat
@@ -0,0 +1,3 @@
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\KillerJimMod\Binaries\Win64\UE4Editor-KillerJimModProxy.dll D:\DVG\Assets\KJModA_VRPDoor\Plugins\KillerJimMod\Binaries\Win64\UE4Editor-KillerJimModProxy.dll
+copy /Y D:\DVG\Perforce\KJModProject\Plugins\KJModEditor\Binaries\Win64\UE4Editor-KJModEditor.dll D:\DVG\Assets\KJModA_VRPDoor\Plugins\KJModEditor\Binaries\Win64\UE4Editor-KJModEditor.dll
+timeout 2
